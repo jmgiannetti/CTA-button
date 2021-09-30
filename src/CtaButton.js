ba96e9b0727eeb27985868ac0d99ce606f17cd76
jmgiannetti <90001880+jmgiannetti@users.noreply.github.com>
2021-09-30 14:58:07 -0400

    Update CtaButton.js

--- src/CtaButton.js
+++ src/CtaButton.js
@@ -5,18 +5,23 @@ import '@lrnwebcomponents/simple-icon/lib/simple-icons.js';
 export class CtaButton extends LitElement {
   static get styles() {
     return css`
+      :host {
+        display: block;
+        padding: 400px;
+        color: var(--cta-button-text-color, #000);
+      }
       .button-wrapper {
         display: block;
         text-align: center;
       }
       .cta-button {
         background-color: #bf40bf;
-        color: #000;
+        color: #000000;
         font-family: copperplate, fantasy;
       }
-      // adding focus to hover command allows button to change color when tabbed to
+
       .cta-button:hover,
-      :focus {
+      .cta-button:focus {
         background-color: #cf9fff;
       }
 
@@ -28,11 +33,6 @@ export class CtaButton extends LitElement {
         font-size: 2em;
         padding: 1em 2em;
         width: auto;
-      }
-      :host {
-        display: block;
-        padding: 400px;
-        color: var(--cta-button-text-color, #000);
       }
     `;
   }
